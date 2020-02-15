27d6371fcdb28a7a9dff24ed37bc82798cfbd691
GabrielNascimento10 <60801918+GabrielNascimento10@users.noreply.github.com>
2020-02-14 22:30:42 -0200

    Initial commit

---
+++ Models/Blog/Postagem/PostagemEntity.cs
@@ -0,0 +1,35 @@
+﻿using Blog.Models.Blog.Autor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models.Blog.Postagem
+{
+    public class PostagemEntity
+    {
+        //FQCN => Fully Qualified Class Name
+        //Blog.Models.Blog.Postagem.PostagemEntity
+
+        //private string Titulo;
+        //protected string Titulo;
+
+        public string Titulo;
+        public AutorEntity Autor;
+
+        public virtual string editar() // Polimorfismo
+        {
+            return "edição realizada";
+        }
+    }
+
+        public class PostagemSobreFilmesEntity : PostagemEntity //Herança
+    {
+        public string Genero; //Encapsolamento
+
+        public override string editar() // Polimorfismo
+        {
+            return "edição realizada na postagem sobre filmes";
+        }
+    }
+}
